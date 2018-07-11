535d1e47461a31a315daed52f0ac1c0509c4bd1c
SongGeorge <465443390@qq.com>
2018-07-11 23:36:07 +0800

    test

---
+++ src/main/java/com/zhousi/spring/boot/blog/springboothelloworld/SpringbootHelloworldApplication.java
@@ -0,0 +1,12 @@
+package com.zhousi.spring.boot.blog.springboothelloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootHelloworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootHelloworldApplication.class, args);
+	}
+}
